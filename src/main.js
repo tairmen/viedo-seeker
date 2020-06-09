e3a91a1635af2f4a2fd53224f2635e8c48799d89
Tair Abduraimov <abduraimovtair@gmail.com>
2020-06-09 13:10:53 +0300

    init project

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import store from "./store";
+import router from "./router";
+import Axios from "axios";
+import vuetify from './plugins/vuetify';
 
-Vue.config.productionTip = false
+Vue.prototype.$http = Axios;
+const token = localStorage.getItem("token");
+if (token) {
+  Vue.prototype.$http.defaults.headers.common["Authorization"] = token;
+}
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  store,
+  router,
+  vuetify,
+  render: (h) => h(App)
+}).$mount("#app");
